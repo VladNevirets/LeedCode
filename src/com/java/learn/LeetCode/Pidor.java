2bd8e616332219a77a9b79133a4ae218453b8324
Vlad Nevirets <neviretsv@gmail.com>
2024-09-13 13:12:44 +0300

    not interesting

---
+++ src/com/java/learn/LeetCode/Pidor.java
@@ -0,0 +1,13 @@
+package com.java.learn.LeetCode;
+
+public class Pidor {
+    public Integer pidorchick(Integer l){
+        return l*l;
+
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
--- src/com/java/learn/MainClass.java
+++ src/com/java/learn/MainClass.java
@@ -3,10 +3,13 @@ package com.java.learn;
 import com.java.learn.LeetCode.BinarySearch.BinarySearch;
 import com.java.learn.LeetCode.BinarySearch.SearchInsertPosition;
 import com.java.learn.LeetCode.BinarySearch.SquareX;
-import com.java.learn.LeetCode.String.ValidParentheses;
+import com.java.learn.LeetCode.Pidor;
+
 
 public class MainClass {
+
     public static void main(String[] args){
+
         BinarySearch bin = new BinarySearch();
         SquareX sq = new SquareX();
 
@@ -14,6 +17,8 @@ public class MainClass {
         int[] nums = {4,5,6,7,0,1,2};
         System.out.println(sq.mySqrt(5));
         System.out.println(bin.binary(nums,0));
-        
+
     }
+
+
 }
