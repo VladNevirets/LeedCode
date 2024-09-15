25f5b9954c443c92a20e05a8711d6efd4103aa27
Vlad Nevirets <neviretsv@gmail.com>
2024-09-15 17:53:03 +0300

    New TwoSum alghorithm

--- src/com/java/learn/LeetCode/TwoSum.java
+++ src/com/java/learn/LeetCode/TwoSum.java
@@ -4,8 +4,10 @@ public class TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
         for(int i=0;i<=nums.length-1;i++){
-            if(nums[i]+nums[i+1]==target){
-                return new int[]{i,i+1};
+            for(int j=i+1;j<=nums.length-1;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i,j};
+                }
 
             }
         }
